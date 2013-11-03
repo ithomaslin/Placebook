88b2d0637954eadd662a0a1240d005cf89be2650
Shaun Dowling <shaunmdowling@gmail.com>
2013-11-03 08:32:05 +0000

    Got code to compile

--- Placebook/TimelineViewController.m
+++ Placebook/TimelineViewController.m
@@ -8,8 +8,6 @@
 
 #import "TimelineViewController.h"
 #import "TimelineCell.h"
-#import "AFNetworking.h"
-#import "AFHTTPRequestOperation.h"
 
 @interface TimelineViewController ()
 
@@ -67,20 +65,6 @@
 -(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
     
-}
-
--(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    if ([[segue identifier] isEqualToString:@"TimelinePush"])
-    {
-        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-        NSDictionary *parameters = @{@"foo": @"bar"};
-        [manager POST:@"http://54.217.128.103:3000/posts" parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
-            NSLog(@"JSON: %@", responseObject);
-        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-            NSLog(@"Error: %@", error);
-        }];
-    }
 }
 
 @end
